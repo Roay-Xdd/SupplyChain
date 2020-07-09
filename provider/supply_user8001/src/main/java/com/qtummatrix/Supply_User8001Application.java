296b3a87aca135c38517c3cc00ab6be68e2b7e17
e <2223486807@qq.com>
2020-07-09 11:33:28 +0800

    更改数据库配置信息  网关

--- provider/supply_user8001/src/main/java/com/qtummatrix/Supply_User8001Application.java
+++ provider/supply_user8001/src/main/java/com/qtummatrix/Supply_User8001Application.java
@@ -4,9 +4,11 @@ package com.qtummatrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class Supply_User8001Application {
     public static void main(String[] args) {
         SpringApplication.run(Supply_User8001Application.class,args);
